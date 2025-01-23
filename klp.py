0de04c90388624b50ca504fa3a6802d517e78604
Dirk Loss <mail@dirk-loss.de>
2025-01-23 21:14:27 +0100

    Bump version to 0.76.0

--- klp.py
+++ klp.py
@@ -62,7 +62,7 @@ from typing import (
 # Type variable for generic types
 T = TypeVar("T")  # Used for generic type hints if needed
 
-__version__ = "0.75.1"
+__version__ = "0.76.0"
 
 INPUT_QUOTE = r"\""
 
